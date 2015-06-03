84af13854620384fe28d304277256c21bffb91d3
Miguel Coronel <m.coronel.seg@gmail.com>
2015-06-03 16:45:48 -0500

    Add get and set urlFormater and create the class UrlFormatter

---
+++ app/models/url_formatter.server.model.js
@@ -0,0 +1,12 @@
+module.exports = {
+  addHttpIfNoPresent: function(url) {
+    if (!url) {
+      return url;
+    } else {
+      if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+        url = 'http://' + url;
+      }
+      return url;
+    }
+  }
+};
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,16 +1,30 @@
 var mongoose = require('mongoose'),
+    urlFormatter = require('../../app/models/url_formatter.server.model'),
     Schema = mongoose.Schema;
     
 var UserSchema = new Schema({
     firstName: String,
     lastName: String,
     email: String,
-    username: String,
+    username: {
+      type: String,
+      trim: true
+    },
     password: String,
     created: {
       type: Date,
       default: Date.now
+    },
+    website: {
+      type: String,
+      set: urlFormatter.addHttpIfNoPresent,
+      get: urlFormatter.addHttpIfNoPresent
     }
   });
 
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+});
+
+UserSchema.set('toJSON', { getters: true , virtuals: true });
 mongoose.model('User', UserSchema);
