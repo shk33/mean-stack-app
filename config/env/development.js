cb98d7f10af7af47f8555b06a0cd8e16d8c4b7af
Miguel Coronel <m.coronel.seg@gmail.com>
2015-06-10 01:19:40 -0500

    Add facebook strategy config

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,9 @@
 module.exports = {
   db: 'mongodb://localhost/mean-book',
-  sessionSecret: 'developmentSessionSecret'
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '1580163918917999',
+    clientSecret: '6c6a7a77b7142a3ba437d3ee96546797',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  }
 };
