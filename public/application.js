1c141ad7f3cb2e687a9a5618928b9ce11a1fd847
Miguel Coronel <m.coronel.seg@gmail.com>
2015-06-13 01:11:22 -0500

    Add example module to app

--- public/application.js
+++ public/application.js
@@ -1,6 +1,7 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+var mainApplicationModule = angular.module(mainApplicationModuleName, 
+    ['example']);
   angular.element(document).ready(function() {
     angular.bootstrap(document, [mainApplicationModuleName]);
 });
