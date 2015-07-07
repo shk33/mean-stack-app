f4b6baf27107a1227b5bc8436859f75400a0cbf4
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-06 23:57:58 -0500

    Configure sockets for a chat and store passport session in Mongo

--- config/express.js
+++ config/express.js
@@ -1,15 +1,20 @@
 var config  = require('./config'),
+    http = require('http'),
+    socketio = require('socket.io'),
     express = require('express'),
     morgan  = require('morgan'),
     compress   = require('compression'),
     bodyParser = require('body-parser'),
     flash = require('connect-flash'),
     session    = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
     methodOverride = require('method-override');
     passport = require('passport');
 
-module.exports = function() {
+module.exports = function(db) {
   var app = express();
+  var server = http.createServer(app);
+  var io = socketio.listen(server);
 
   if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -23,10 +28,15 @@ module.exports = function() {
   app.use(bodyParser.json());
   app.use(methodOverride());
 
+  var mongoStore = new MongoStore({
+    db: db.connection.db
+  });
+
   app.use(session({
     saveUninitialized: true,
     resave: true,
-    secret: config.sessionSecret
+    secret: config.sessionSecret,
+    store: mongoStore
   }));
 
   app.set('views', './app/views');
@@ -42,5 +52,6 @@ module.exports = function() {
 
   app.use(express.static('./public'));
 
-  return app;
+  require('./socketio')(server,io,mongoStore);
+  return server;
 };
