34f37fbc4eae97dfca7ebf336da8e5813ce14b1b
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-08 02:30:24 -0500

    Add Mocha testing and modify README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A mean stack app for learning porpuses.
 The code is following the book 
 [Packtup "MEAN web development"](https://www.packtpub.com/web-development/mean-web-development)
+
+For Testing Express code just run this command
+```
+NODE_ENV=test mocha --reporter spec app/tests
+``` 
---
+++ config/env/test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  db: 'mongodb://localhost/mean-book-test',
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: '1580163918917999',
+    clientSecret: '6c6a7a77b7142a3ba437d3ee96546797',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  },
+  twitter: {
+    clientID: '2787003818',
+    clientSecret: 'TYjJdit0Z9qTxUTdQJLqAyv9UxGFkbQfeNtri9tK5Sny20YLxB',
+    callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+  }
+};
